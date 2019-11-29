2659c550f57f2b9cec604e52bed954eac15c5a45
Son Dinh <peaceman77@hotmail.com>
2019-11-29 14:52:05 +0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `myjtj4a3wodw`,
+        accessToken: `TwhB0Ag1PaHhV240fkzaROeel6V2icn2A5_TtRltw9I`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
